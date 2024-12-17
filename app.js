0da78c9df40e5db06351dad56f5d51ebb43144d1
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-12-17 08:11:25 +0100

    Metody i podpinanie ich w wąsach - dynamiczne generowanie danych

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and learn EVERYTHING!',
             vueLink: 'https://vuejs.org/'
         };
+    },
+    methods: { //zawsze obiekt
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'LEarn Vue';
+            } else {
+                return 'Ale master VUE';
+            }
+        }
     }
 });
 
